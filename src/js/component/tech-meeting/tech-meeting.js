0e56231dddb1c419eab568ffaa71ea28e48cc1f2
fahim chowdhury <fahim@gmx.com>
2017-07-24 11:09:38 +0100

    fixed duplicate calls

--- src/js/component/tech-meeting/tech-meeting.js
+++ src/js/component/tech-meeting/tech-meeting.js
@@ -5,13 +5,14 @@ const TechMeetingComponent = {
   topicIndex: 0,
   itemIndex: 0,
   currentTopic: '',
+  isStarted: true,
   run(question, obj, card) {
     Speech.component = TechMeetingComponent;
     TechMeetingService.get(this.onMeetingNotes.bind(this));
     Speech.speak('okay. hello everyone, welcome to the tech meeting. I\'m going to get the meeting notes so please give me a second');
   },
   execute(response) {
-    if (this.findMatch(response)) {
+    if (this.findMatch(response) && this.isStarted) {
       this.next();
     }
   },
@@ -20,7 +21,7 @@ const TechMeetingComponent = {
     if (this.itemIndex >= TechMeetingService.data[this.currentTopic].length) {
       this.itemIndex = 0;
       this.topicIndex++;
-      if(this.getTopic())Speech.speak('let\'s move on to ' + this.getTopic() + ' topics');
+      if(this.getTopic())Speech.speak('let\'s move to ' + this.getTopic() + ' topics');
     }
     if (this.topicIndex >= Object.keys(TechMeetingService.data).length) {
       Speech.speak('that concludes this tech meeting. thank you.');
@@ -32,14 +33,17 @@ const TechMeetingComponent = {
     }
   },
   onMeetingNotes() {
-    Speech.speak('let\'s start with the news');
+    Speech.speak('let\'s start with the news.');
     this.currentTopic = this.getTopic();
     let item = TechMeetingService.data[this.currentTopic][this.itemIndex];
+    setTimeout(()=>{
+       this.isStarted=true;
+    },5000);
     this.say(this.currentTopic, item);
 
   },
   say(topic, item) {
-    Speech.speak(topic + 'topic point ' + (this.itemIndex + 1) + '. ' + item.TITLE + '. ' + item.SPEAKER + ' can you tell us about this?');
+    Speech.speak(topic.toLowerCase() + 'topic point ' + (this.itemIndex + 1) + '. ' + item.TITLE + '. ' + item.SPEAKER + ' can you tell us about this?');
   },
   getTopic() {
     switch (this.topicIndex) {
